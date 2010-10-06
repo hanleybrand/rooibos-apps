85f2b3851af435e409e1dcbab37eb5cec2e23d67
Andreas Knab <knab2ar@jmu.edu>
2010-10-06 13:44:30 -0400

    Add OVC redirects for MDID2 direct links

--- ovc/urls.py
+++ ovc/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
-from views import redirect_to_video
+from views import redirect_to_video, redirect_to_video_id
 
 urlpatterns = patterns('',
      url(r'^(?P<id>[-\w]+)/$', redirect_to_video, name='ovc-redirect'),
+     url(r'^id/(?P<id>[\d]+)/$', redirect_to_video_id, name='ovc-redirect-id'),
     )
--- ovc/views.py
+++ ovc/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.contenttypes.models import ContentType
 from rooibos.data.models import Collection, standardfield, FieldValue, Record
 from rooibos.viewers import FULL_SUPPORT
 from rooibos.viewers.viewers.videoplayer import VideoPlayer
+from rooibos.migration.models import ObjectHistory
+
+
 
 @login_required
 def redirect_to_video(request, id):
@@ -13,3 +17,12 @@ def redirect_to_video(request, id):
     if not records:
         raise Http404()
     return HttpResponseRedirect(VideoPlayer().url_for_obj(records[0]))
+
+
+@login_required
+def redirect_to_video_id(request, id):
+    try:
+        record = ObjectHistory.objects.get(content_type=ContentType.objects.get_for_model(Record),original_id=81675).content_object
+    except ObjectHistory.DoesNotExist:
+        raise Http404()
+    return HttpResponseRedirect(VideoPlayer().url_for_obj(record))
