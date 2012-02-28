c875c427f7e068c1f84851d067d16e09a3649968
Andreas Knab <knab2ar@jmu.edu>
2012-02-28 13:18:59 -0500

    Allow periods in OVC identifiers

--- ovc/urls.py
+++ ovc/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 from views import redirect_to_video, redirect_to_video_id
 
 urlpatterns = patterns('',
-     url(r'^(?P<id>[-\w]+)/$', redirect_to_video, name='ovc-redirect'),
+     url(r'^(?P<id>[-\w.]+)/$', redirect_to_video, name='ovc-redirect'),
      url(r'^id/(?P<id>[\d]+)/$', redirect_to_video_id, name='ovc-redirect-id'),
 
      url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.OVC_STATIC_FILES}, name='ovc-static'),
